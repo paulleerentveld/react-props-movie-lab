e17bc70ddf1d1f607b250bb704b3447d8e9b2f52
paulleerentveld <paul@veldnet.com.au>
2022-05-05 16:18:32 +1000

    lab

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
+
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
+    
+    return movieData.map((item,index) => <MovieCard 
+      key={index}
+      title={item.title} 
+      IMDBRating={item.IMDBRating} 
+      genres={item.genres} 
+      poster={item.poster} />)
     // map over your movieData array and return an array of the correct JSX
   }
 
@@ -12,6 +20,7 @@ export default class MovieShowcase extends Component {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
+        
       </div>
     )
   }
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -12,6 +12,7 @@ import ttm from '../assets/poster-imgs/the-trash-man.png'
 import React, { Component } from 'react';
 import CardFront from './CardFront.js';
 import CardBack from './CardBack.js';
+import MovieShowcase from '../MovieShowcase'
 
 
 const posterMap = {
@@ -27,17 +28,33 @@ const posterMap = {
   'default': defaultPoster
 }
 
+
 export default class MovieCard extends Component {
+
+  generateUrl = () => {
+    
+    return posterMap[this.props.poster];
+    // implement meeeee! See the readme for instructions
+  }
 
   render() {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        {/* <CardFront image={this.generateUrl()}  /> */}
+        <CardFront image={this.generateUrl()}  />
+        <CardBack title={this.props.title} genres={this.props.genres} IMDB={this.props.IMDBRating} />
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+
+MovieCard.defaultProps = {
+  title: "Unknown",
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
+  
+}
